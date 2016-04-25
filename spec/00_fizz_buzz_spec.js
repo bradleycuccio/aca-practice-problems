aa05a86f601819d4c908d484b9f8a123bb529e90
Canaan Davis <canaandavis@gmail.com>
2016-04-25 17:35:56 -0500

    Add comments to first problem

--- spec/00_fizz_buzz_spec.js
+++ spec/00_fizz_buzz_spec.js
@@ -1,17 +1,27 @@
 var fizzBuzz = require ('../src/00_fizz_buzz');
 
+// Write a function `fizzBuzz` that takes two numbers and outputs an array
+// of strings of the values, Fizz, Buzz, and FizzBuzz.
+
+// The function should use the two numbers as a range and for any number,
+// included in that range, that is divisible by 3 the string Fizz should be
+// added to the array, Buzz for numbers divisible by 5 and
+// FizzBuzz for numbers divisible by 3 and 5.
+
+// Any number that is not divisible by 3 or 5 should not add to the array
+
 describe("Fizz Buzz", function() {
-  it("outputs Fizz for numbers divisable by 3", function() {
+  it("outputs Fizz for numbers divisible by 3", function() {
     var result = fizzBuzz(3, 3);
     expect(result).toEqual(['Fizz']);
   });
 
-  it("outputs Fizz for numbers divisable by 3", function() {
+  it("outputs Fizz for numbers divisible by 3", function() {
     var result = fizzBuzz(5, 5);
     expect(result).toEqual(['Buzz']);
   });
 
-  it("outputs FizzBuzz for numbers divisable by 3 and 5", function() {
+  it("outputs FizzBuzz for numbers divisible by 3 and 5", function() {
     var result = fizzBuzz(15, 15);
     expect(result).toEqual(['FizzBuzz']);
   });
